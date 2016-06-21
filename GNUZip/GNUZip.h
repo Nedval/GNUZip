71c490ccdad1f4965b684c8ccf171e8cb5bec50f
Jrting <Jrting@Nedval.me>
2016-06-21 14:43:02 +0800

    Fix header

--- GNUZip/GNUZip.h
+++ GNUZip/GNUZip.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char GNUZipVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GNUZip/PublicHeader.h>
 
+#import <GNUZip/Zip.h>
